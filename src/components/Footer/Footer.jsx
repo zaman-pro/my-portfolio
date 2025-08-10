f8601d2c134cb33c337857e5dfb807b1b0cf98ff
unknown <zaman-pro>
2025-08-11 05:25:46 +0600

    add simple navbar

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,9 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="bg-emerald-900 text-white text-8xl text-center">
-      this is footer
-    </div>
+    <div className="bg-emerald-900 text-white text-8xl text-center">Footer</div>
   );
 };
 
