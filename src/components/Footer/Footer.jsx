7fb7248d26a1f3e5f06cbde62835fefac7c097a0
unknown <zaman-pro>
2025-08-12 19:29:05 +0600

    add footer section

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,19 @@
 import React from "react";
+import { ArrowUp } from "lucide-react";
 
 const Footer = () => {
   return (
-    <div className="bg-emerald-900 text-white text-8xl text-center">Footer</div>
+    <footer className="py-12 px-4 bg-card relative border-t border-border mt-12 pt-8 flex flex-wrap justify-between items-center">
+      <p className="text-sm">
+        &copy; {new Date().getFullYear()} ZamanPro.co All rights reserved.
+      </p>
+      <a
+        href="#hero"
+        className="p-2 rounded-full bg-primary/10 hover:bg-primary/20 text-primary transition-colors"
+      >
+        <ArrowUp size={20} />
+      </a>
+    </footer>
   );
 };
 
